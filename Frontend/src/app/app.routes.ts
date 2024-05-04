6b4bedd66794678ba4722b3245d29503987061e4
Juan Sebastian Ortiz Ramirez <94332942+Juanse1425@users.noreply.github.com>
2024-05-04 16:06:53 -0500

    V 0.3.6 - Se corrige un error en el archivo app.routes.ts que sin querer se habia puesto una tabulacion en el nombre del componente de registro

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { ListaProductosComponent } from '../shop/lista-productos/lista-productos.component';
 import { CarritoComprasComponent } from '../shop/carrito-compras/carrito-compras.component';
 import { LoginComponent } from './login/login.component';
-import { Registro   Component } from './registro/registro.component';
+import { RegistroComponent } from './registro/registro.component';
 import { DetalleProductoComponent } from './detalle-producto/detalle-producto.component';
 import { CrearClienteComponent } from './clientes/crear-cliente/crear-cliente.component';
 import { ListaClientesComponent } from './clientes/lista-clientes/lista-clientes.component';
